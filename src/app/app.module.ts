b19cf36db2c16c90742a01ea211898340e241bab
sush-smile08 <sushmitashikha8@gmail.com>
2018-05-13 17:23:32 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { ListService } from './list/list.service';
+import { CategoryPipe } from './list/list.pipe';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    CategoryPipe
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [
     ListService
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,26 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import {  ListService } from './list.service';
+import { Component, OnInit, Input } from '@angular/core';
 import { Config } from 'protractor';
+import {  ListService } from './list.service';
+import { CategoryPipe } from './list.pipe';
+
+@Input()
 
 @Component({
   selector: 'list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
-  providers: [ ListService ]
+  providers: [ ListService ] ,
+  pipes: [CategoryPipe]
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
   tittle = "List of Games";
   appsName ;
   headers: string[];
   config ;
+  isDesc: boolean = false;
+  column: string ='title';
+
+  ngOnInit(){  }
 
   constructor(private ListService: ListService) {
     this.showConfigResponse();
   }
 
+
+
   showConfig() {
     this.ListService.getlist()
-      .subscribe((data: Config) => this.config = {
+      .subscribe((data: Config) => this.appsName = {
         heroesUrl: data['heroesUrl']
     });
   }
@@ -43,8 +53,26 @@ export class ListComponent {
         console.log(this.config.heroesUrl);
       });
   }
+
+  sort(property){
+    this.isDesc = !this.isDesc; //change the direction    
+    this.column = property;
+    let direction = this.isDesc ? 1 : -1;
+
+    this.appsName.sort(function(a, b){
+        if(a[property] < b[property]){
+            return -1 * direction;
+        }
+        else if( a[property] > b[property]){
+            return 1 * direction;
+        }
+        else{
+            return 0;
+        }
+    });
+};
   
-  clickMe(){
-    return this.config.sort((a, b) => a.title.localCompare(b.title));
-  }
+  //clickMe(){
+    //return appsName.sort((a, b) => a.title.localCompare(b.title));
+  //}
 }
